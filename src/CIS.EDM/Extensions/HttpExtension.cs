72a1f081653a36d6cf34de5dfa23094759f83de1
o.nadymov <o.nadymov@spoleto.ru>
2023-11-24 12:20:13 +0300

    isZipResponse

--- src/CIS.EDM/Extensions/HttpExtension.cs
+++ src/CIS.EDM/Extensions/HttpExtension.cs
@@ -4,18 +4,21 @@ using System.Net.Http.Headers;
 
 namespace CIS.EDM.Extensions
 {
-    public static class HttpExtension
-    {
-        public static void ConfigureRequestMessage(this HttpRequestMessage requestMessage)
-        {
-            requestMessage.Headers.Accept.Clear();
-            requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(DefaultSettings.ContentType));
-            requestMessage.Headers.AcceptCharset.ParseAdd(DefaultSettings.Charset);
-        }
+	public static class HttpExtension
+	{
+		public static void ConfigureRequestMessage(this HttpRequestMessage requestMessage, bool isZipResponse = false)
+		{
+			requestMessage.Headers.Accept.Clear();
 
-        public static void ConfigureHttpClient(this HttpClient client)
-        {
-            client.Timeout = new TimeSpan(0, 0, 5, 0);
-        }
-    }
+			var contentType = isZipResponse ? ContentType.Zip : DefaultSettings.ContentType;
+			requestMessage.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(contentType));
+
+			requestMessage.Headers.AcceptCharset.ParseAdd(DefaultSettings.Charset);
+		}
+
+		public static void ConfigureHttpClient(this HttpClient client)
+		{
+			client.Timeout = new TimeSpan(0, 0, 5, 0);
+		}
+	}
 }
