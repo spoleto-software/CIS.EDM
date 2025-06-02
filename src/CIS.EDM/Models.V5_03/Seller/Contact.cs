052e91f23f31c68c690f20eef9d0de6ef345bd89
o.nadymov <o.nadymov@spoleto.ru>
2025-06-02 10:46:58 +0300

    Contact 5.03

--- src/CIS.EDM/Models.V5_03/Seller/Contact.cs
+++ src/CIS.EDM/Models.V5_03/Seller/Contact.cs
@@ -1,4 +1,6 @@
-﻿namespace CIS.EDM.Models.V5_03.Seller
+﻿using System.Collections.Generic;
+
+namespace CIS.EDM.Models.V5_03.Seller
 {
 	/// <summary>
 	/// <see href="https://www.consultant.ru/document/cons_doc_LAW_468229/d552ee61efc831d3b939a200296fc6f907a34ab2/">Контактные данные.</see>
@@ -10,17 +12,23 @@
         /// Номер контактного телефона/факс.
         /// </summary>
         /// <value><b>Тлф</b> - сокращенное наименование (код) элемента.</value>
-        public string Phone { get; set; }
+        public List<string> PhoneList { get; set; }
 
         /// <summary>
         /// Адрес электронной почты.
         /// </summary>
         /// <value><b>ЭлПочта</b> - сокращенное наименование (код) элемента.</value>
-        public string Email { get; set; }
+        public List<string> EmailList { get; set; }
 
-        /// <summary>
-        /// Текстовое представление объекта.
-        /// </summary>
-        public override string ToString() => $"Phone: {Phone}. Email: {Email}";
+		/// <summary>
+		/// Иные контактные данные
+		/// </summary>
+		/// <value><b>ИнКонт</b> - сокращенное наименование (код) элемента.</value>
+		public string OtherInfo { get; set; }
+
+		/// <summary>
+		/// Текстовое представление объекта.
+		/// </summary>
+		public override string ToString() => $"Phone: {string.Join(',', PhoneList)}. Email: {string.Join(',', EmailList)}";
     }
 }
